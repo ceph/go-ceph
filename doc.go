0c6bdf8d590fdc4afa07c2bb82e1ad5e8890307c
Kai Storbeck <kai@xs4all.nl>
2015-02-11 22:21:05 +0100

    run gofmt on all sources

--- doc.go
+++ doc.go
@@ -4,6 +4,6 @@ Set of wrappers around Ceph APIs.
 package rados
 
 import (
-    _ "github.com/noahdesu/go-ceph/rados"
-    _ "github.com/noahdesu/go-ceph/rbd"
+	_ "github.com/noahdesu/go-ceph/rados"
+	_ "github.com/noahdesu/go-ceph/rbd"
 )
--- package_test.go
+++ package_test.go
@@ -1,12 +1,12 @@
 package rados
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestImports(t *testing.T) {
-    if assert.Equal(t, 1, 1) != true {
-        t.Error("Something is wrong.")
-    }
+	if assert.Equal(t, 1, 1) != true {
+		t.Error("Something is wrong.")
+	}
 }
