b01b7743ec30756f6e5db1d21e4f5fdce0c3c8f4
Connor Gorman <cgorman@medallia.com>
2016-09-23 10:55:35 -0700

    Add support for checking if in blacklist

--- api/osd_blacklist_ls.go
+++ api/osd_blacklist_ls.go
@@ -1,9 +1,22 @@
 package api
 
+import (
+	"strings"
+)
+
 type OsdBlacklistLs struct {
 	Nodes []struct {
 		Addr  string `json:"addr"`
 		Until string `json:"until"`
 	} `json:"output"`
 	Status string `json:"status"`
+}
+
+func (obl *OsdBlacklistLs) IsInBlacklist(ip string) bool {
+	for _, node := range obl.Nodes {
+		if strings.Contains(node.Addr, ip) {
+			return true
+		}
+	}
+	return false
 }
