efe2f6b97ca515e6eac972767b6c6a92142e8bd1
Niels de Vos <ndevos@redhat.com>
2020-01-28 10:42:33 +0100

    rbd: replace deprecated rbd_list() with rbd_list2()
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- rbd/rbd.go
+++ rbd/rbd.go
@@ -203,30 +203,6 @@ func Version() (int, int, int) {
 	return int(c_major), int(c_minor), int(c_patch)
 }
 
-// GetImageNames returns the list of current RBD images.
-func GetImageNames(ioctx *rados.IOContext) (names []string, err error) {
-	buf := make([]byte, 4096)
-	for {
-		size := C.size_t(len(buf))
-		ret := C.rbd_list(C.rados_ioctx_t(ioctx.Pointer()),
-			(*C.char)(unsafe.Pointer(&buf[0])), &size)
-		if ret == -C.ERANGE {
-			buf = make([]byte, size)
-			continue
-		} else if ret < 0 {
-			return nil, RBDError(ret)
-		}
-		tmp := bytes.Split(buf[:size-1], []byte{0})
-		for _, s := range tmp {
-			if len(s) > 0 {
-				name := C.GoString((*C.char)(unsafe.Pointer(&s[0])))
-				names = append(names, name)
-			}
-		}
-		return names, nil
-	}
-}
-
 // GetImage gets a reference to a previously created rbd image.
 func GetImage(ioctx *rados.IOContext, name string) *Image {
 	return &Image{
