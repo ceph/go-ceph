722684f6b2203724c5204df9055cfed89b402fc7
John Mulligan <jmulligan@redhat.com>
2020-02-03 13:25:14 -0500

    rbd: rename error handling helper func to make it private
    Similar to recent changes for rados (df46476e) there is no good reason
    for the errorno to go-style error function to be exported from the
    package. Decapitalize it.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- rbd/rbd_test.go
+++ rbd/rbd_test.go
@@ -25,14 +25,14 @@ func GetUUID() string {
 }
 
 func TestRBDError(t *testing.T) {
-	err := GetError(0)
+	err := getError(0)
 	assert.NoError(t, err)
 
-	err = GetError(-39) // NOTEMPTY (image still has a snapshot)
+	err = getError(-39) // NOTEMPTY (image still has a snapshot)
 	assert.Error(t, err)
 	assert.Equal(t, err.Error(), "rbd: ret=39, Directory not empty")
 
-	err = GetError(345) // no such errno
+	err = getError(345) // no such errno
 	assert.Error(t, err)
 	assert.Equal(t, err.Error(), "rbd: ret=345")
 }
