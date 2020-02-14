7e57950138339c0a38fafa11589987e993c97a45
John Mulligan <jmulligan@redhat.com>
2020-02-14 15:11:06 -0500

    cephfs: add a RewindDir and List function for simpler dir listings
    The RewindDir is used by List and can be used independently. It
    resets the open dir to the "start".
    The List function and the accompanying DirEntries type allow one
    to simply grab all the dir entries at once. The Names method on the
    DirEntries type allows the user to get the most commonly wanted items
    from the dir entries in one shot.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- cephfs/directory.go
+++ cephfs/directory.go
@@ -92,3 +92,50 @@ func (dir *Directory) ReadDir() (*DirEntry, error) {
 	}
 	return toDirEntry(&de), nil
 }
+
+// RewindDir sets the directory stream to the beginning of the directory.
+//
+// Implements:
+//  void ceph_rewinddir(struct ceph_mount_info *cmount, struct ceph_dir_result *dirp);
+func (dir *Directory) RewindDir() {
+	C.ceph_rewinddir(dir.mount.mount, dir.dir)
+}
+
+// dirEntries provides a convenient wrapper around slices of DirEntry items.
+// For example, use the Names() call to easily get only the names from a
+// DirEntry slice.
+type dirEntries []*DirEntry
+
+// list returns all the contents of a directory as a dirEntries slice.
+//
+// list is implemented using ReadDir. If any of the calls to ReadDir returns
+// an error List will return an error. However, all previous entries
+// collected will still be returned. Callers of this function may want to check
+// the entries return value even when an error is returned.
+// List rewinds the handle every time it is called to get a full
+// listing of directory contents.
+func (dir *Directory) list() (dirEntries, error) {
+	var (
+		err     error
+		entry   *DirEntry
+		entries = make(dirEntries, 0)
+	)
+	dir.RewindDir()
+	for {
+		entry, err = dir.ReadDir()
+		if err != nil || entry == nil {
+			break
+		}
+		entries = append(entries, entry)
+	}
+	return entries, err
+}
+
+// names returns a slice of only the name fields from dir entries.
+func (entries dirEntries) names() []string {
+	names := make([]string, len(entries))
+	for i, v := range entries {
+		names[i] = v.Name()
+	}
+	return names
+}
--- cephfs/directory_test.go
+++ cephfs/directory_test.go
@@ -95,3 +95,68 @@ func TestReadDir(t *testing.T) {
 		assert.Subset(t, found, subdirs)
 	})
 }
+
+func TestDirectoryList(t *testing.T) {
+	mount := fsConnect(t)
+	defer fsDisconnect(t, mount)
+
+	dir1 := "/base"
+	err := mount.MakeDir(dir1, 0755)
+	assert.NoError(t, err)
+	defer func() { assert.NoError(t, mount.RemoveDir(dir1)) }()
+
+	subdirs := []string{"a", "bb", "ccc", "dddd"}
+	for _, s := range subdirs {
+		spath := dir1 + "/" + s
+		err = mount.MakeDir(spath, 0755)
+		assert.NoError(t, err)
+		defer func(d string) {
+			assert.NoError(t, mount.RemoveDir(d))
+		}(spath)
+	}
+
+	t.Run("root", func(t *testing.T) {
+		dir, err := mount.OpenDir("/")
+		assert.NoError(t, err)
+		assert.NotNil(t, dir)
+		defer func() { assert.NoError(t, dir.Close()) }()
+
+		entries, err := dir.list()
+		assert.NoError(t, err)
+		assert.Greater(t, len(entries), 1)
+		found := entries.names()
+		assert.Contains(t, found, "base")
+	})
+	t.Run("dir1", func(t *testing.T) {
+		dir, err := mount.OpenDir(dir1)
+		assert.NoError(t, err)
+		assert.NotNil(t, dir)
+		defer func() { assert.NoError(t, dir.Close()) }()
+
+		entries, err := dir.list()
+		assert.NoError(t, err)
+		assert.Greater(t, len(entries), 1)
+		found := entries.names()
+		assert.Subset(t, found, subdirs)
+	})
+	t.Run("dir1Twice", func(t *testing.T) {
+		dir, err := mount.OpenDir(dir1)
+		assert.NoError(t, err)
+		assert.NotNil(t, dir)
+		defer func() { assert.NoError(t, dir.Close()) }()
+
+		entries, err := dir.list()
+		assert.NoError(t, err)
+		assert.Greater(t, len(entries), 1)
+		found := entries.names()
+		assert.Subset(t, found, subdirs)
+
+		// verify that calling list gives a complete list
+		// even after being used for the same directory already
+		entries, err = dir.list()
+		assert.NoError(t, err)
+		assert.Greater(t, len(entries), 1)
+		found = entries.names()
+		assert.Subset(t, found, subdirs)
+	})
+}
