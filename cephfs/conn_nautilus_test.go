fa5bb0fd69324b6d2fbd32fbd9641b6beb717a80
John Mulligan <jmulligan@redhat.com>
2020-02-14 10:00:21 -0500

    cephfs: add GetFsCid to implement ceph_get_fs_cid
    This function can be used to uniquely id the file system that the
    mount object is connecting to.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ cephfs/conn_nautilus_test.go
@@ -0,0 +1,32 @@
+// +build !luminous,!mimic
+
+package cephfs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetFsCid(t *testing.T) {
+	t.Run("unmounted", func(t *testing.T) {
+		mount, err := CreateMount()
+		require.NoError(t, err)
+		require.NotNil(t, mount)
+
+		err = mount.ReadDefaultConfigFile()
+		require.NoError(t, err)
+
+		cid, err := mount.GetFsCid()
+		assert.Error(t, err)
+		assert.Equal(t, cid, int64(0))
+	})
+	t.Run("mounted", func(t *testing.T) {
+		mount := fsConnect(t)
+
+		cid, err := mount.GetFsCid()
+		assert.NoError(t, err)
+		assert.GreaterOrEqual(t, cid, int64(0))
+	})
+}
