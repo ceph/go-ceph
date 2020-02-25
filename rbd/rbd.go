b02a1565630370005aa827f13dc67788e7d77629
John Mulligan <jmulligan@redhat.com>
2020-02-25 14:32:03 -0500

    rbd: add doc comments to error values
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- rbd/rbd.go
+++ rbd/rbd.go
@@ -49,11 +49,20 @@ const (
 type RBDError int
 
 var (
-	ErrNoIOContext    = errors.New("RBD image does not have an IOContext")
-	ErrNoName         = errors.New("RBD image does not have a name")
+	// ErrNoIOContext may be returned if an api call requires an IOContext and
+	// it is not provided.
+	ErrNoIOContext = errors.New("RBD image does not have an IOContext")
+	// ErrNoName may be returned if an api call requires a name and it is
+	// not provided.
+	ErrNoName = errors.New("RBD image does not have a name")
+	// ErrSnapshotNoName may be returned if an aip call requires a snapshot
+	// name and it is not provided.
 	ErrSnapshotNoName = errors.New("RBD snapshot does not have a name")
-	ErrImageNotOpen   = errors.New("RBD image not open")
-	ErrNotFound       = errors.New("RBD image not found")
+	// ErrImageNotOpen may be returnened if an api call requires an open image handle and one is not provided.
+	ErrImageNotOpen = errors.New("RBD image not open")
+	// ErrNotFound may be returned from an api call when the requested item is
+	// missing.
+	ErrNotFound = errors.New("RBD image not found")
 
 	// retained for compatibility with old versions
 	RbdErrorImageNotOpen = ErrImageNotOpen
