7f7b45b08a423f320c83382065f0a6aa79dc2c95
John Mulligan <jmulligan@redhat.com>
2020-03-25 09:45:53 -0400

    cephfs: split basic path management funcs into new files
    Continue organizing the cephfs functionality by creating new files
    for the most basic path management functions:
      MakeDir, RemoveDir, ChangeDir, CurrentDir
    Similarly, the dedicated test functions for those items are moved into
    a new file as well.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- cephfs/cephfs.go
+++ cephfs/cephfs.go
@@ -155,39 +155,6 @@ func (mount *MountInfo) SyncFs() error {
 	return getError(ret)
 }
 
-// CurrentDir gets the current working directory.
-func (mount *MountInfo) CurrentDir() string {
-	cDir := C.ceph_getcwd(mount.mount)
-	return C.GoString(cDir)
-}
-
-// ChangeDir changes the current working directory.
-func (mount *MountInfo) ChangeDir(path string) error {
-	cPath := C.CString(path)
-	defer C.free(unsafe.Pointer(cPath))
-
-	ret := C.ceph_chdir(mount.mount, cPath)
-	return getError(ret)
-}
-
-// MakeDir creates a directory.
-func (mount *MountInfo) MakeDir(path string, mode uint32) error {
-	cPath := C.CString(path)
-	defer C.free(unsafe.Pointer(cPath))
-
-	ret := C.ceph_mkdir(mount.mount, cPath, C.mode_t(mode))
-	return getError(ret)
-}
-
-// RemoveDir removes a directory.
-func (mount *MountInfo) RemoveDir(path string) error {
-	cPath := C.CString(path)
-	defer C.free(unsafe.Pointer(cPath))
-
-	ret := C.ceph_rmdir(mount.mount, cPath)
-	return getError(ret)
-}
-
 // Chmod changes the mode bits (permissions) of a file/directory.
 func (mount *MountInfo) Chmod(path string, mode uint32) error {
 	cPath := C.CString(path)
