c34501fc6a507e7c2447c3ebd0b241e17c3ae061
John Mulligan <jmulligan@redhat.com>
2020-06-11 14:37:51 -0400

    cephfs: add a ParseConfigArgv function
    The ParseConfigArgv behaves similarly to the same named function in
    rados. The command line argv value is parsed by ceph libs and used to
    configure the mount object.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- cephfs/cephfs.go
+++ cephfs/cephfs.go
@@ -70,6 +70,28 @@ func (mount *MountInfo) ReadDefaultConfigFile() error {
 	return getError(ret)
 }
 
+// ParseConfigArgv configures the mount using a unix style command line
+// argument vector.
+//
+// Implements:
+//  int ceph_conf_parse_argv(struct ceph_mount_info *cmount, int argc, const char **argv);
+func (mount *MountInfo) ParseConfigArgv(argv []string) error {
+	if err := mount.validate(); err != nil {
+		return err
+	}
+	if len(argv) == 0 {
+		return ErrEmptyArgument
+	}
+	cargv := make([]*C.char, len(argv))
+	for i := range argv {
+		cargv[i] = C.CString(argv[i])
+		defer C.free(unsafe.Pointer(cargv[i]))
+	}
+
+	ret := C.ceph_conf_parse_argv(mount.mount, C.int(len(cargv)), &cargv[0])
+	return getError(ret)
+}
+
 // SetConfigOption sets the value of the configuration option identified by
 // the given name.
 //
