3fdf48fb12c1ca3d1ffa5d37d20f5f269feceac4
Mudit Agarwal <muagarwa@redhat.com>
2020-06-25 17:20:46 +0530

    rbd: add wrapper for function rbd_snap_get_trash_namespace()
    Added a wrapper for function rbd_snap_get_trash_namespace() which
    returns the original name of the snapshot which was moved to Trash.
    
    Signed-off-by: Mudit Agarwal <muagarwa@redhat.com>

--- rbd/snapshot_namespace_test.go
+++ rbd/snapshot_namespace_test.go
@@ -109,3 +109,92 @@ func TestGetSnapNamespaceType(t *testing.T) {
 		assert.Equal(t, nsType, SnapNamespaceTypeTrash)
 	})
 }
+
+func TestGetSnapTrashNamespace(t *testing.T) {
+	conn := radosConnect(t)
+	defer conn.Shutdown()
+
+	poolname := GetUUID()
+	err := conn.MakePool(poolname)
+	require.NoError(t, err)
+	defer conn.DeletePool(poolname)
+
+	ioctx, err := conn.OpenIOContext(poolname)
+	require.NoError(t, err)
+	defer ioctx.Destroy()
+
+	imageName := "parent"
+	snapName := "mySnap"
+	cloneName := "myClone"
+	options := NewRbdImageOptions()
+	defer options.Destroy()
+	err = options.SetUint64(ImageOptionOrder, uint64(testImageOrder))
+	assert.NoError(t, err)
+	err = options.SetUint64(ImageOptionFeatures, 1)
+	assert.NoError(t, err)
+
+	err = CreateImage(ioctx, imageName, testImageSize, options)
+	assert.NoError(t, err)
+
+	img, err := OpenImage(ioctx, imageName, NoSnapshot)
+	assert.NoError(t, err)
+	defer func() {
+		assert.NoError(t, img.Close())
+		assert.NoError(t, img.Remove())
+	}()
+
+	snapshot, err := img.CreateSnapshot(snapName)
+	assert.NoError(t, err)
+
+	optionsClone := NewRbdImageOptions()
+	defer optionsClone.Destroy()
+	err = optionsClone.SetUint64(ImageOptionCloneFormat, 2)
+	assert.NoError(t, err)
+
+	// Create a clone of the image using the same snapshot.
+	err = CloneImage(ioctx, imageName, snapName, ioctx, cloneName, optionsClone)
+	assert.NoError(t, err)
+	defer func() { assert.NoError(t, RemoveImage(ioctx, cloneName)) }()
+
+	// Check the name of the snapshot.
+	snapInfoList, err := img.GetSnapshotNames()
+	assert.NoError(t, err)
+	snapInfo := snapInfoList[0]
+	assert.Equal(t, snapInfo.Name, snapName)
+
+	// Remove the snapshot.
+	err = snapshot.Remove()
+	assert.NoError(t, err)
+
+	// Snapshot would move to the trash because linked clone is still there.
+	nsType, err := img.GetSnapNamespaceType(snapInfo.Id)
+	assert.NoError(t, err)
+	assert.Equal(t, nsType, SnapNamespaceTypeTrash)
+
+	// Get the snap info again, name would have changed.
+	newSnapInfoList, err := img.GetSnapshotNames()
+	assert.NoError(t, err)
+	newSnapInfo := newSnapInfoList[0]
+	assert.NotEqual(t, newSnapInfo.Name, snapName)
+	// ID would have remained same.
+	assert.Equal(t, snapInfo.Id, newSnapInfo.Id)
+
+	// Get the original name.
+	origSnapName, err := img.GetSnapTrashNamespace(newSnapInfo.Id)
+	assert.NoError(t, err)
+	assert.Equal(t, snapName, origSnapName)
+
+	invalidSnapID := uint64(22)
+
+	t.Run("InvalidSnapID", func(t *testing.T) {
+		_, err := img.GetSnapTrashNamespace(invalidSnapID)
+		assert.Error(t, err)
+	})
+
+	t.Run("InvalidImage", func(t *testing.T) {
+		invalidImgName := GetUUID()
+		invalidImg := GetImage(ioctx, invalidImgName)
+		_, err := invalidImg.GetSnapTrashNamespace(invalidSnapID)
+		assert.Error(t, err)
+	})
+}
