603a5193c8c10b3e58dd33c1a80e9175b045c56e
Sven Anderson <sven@redhat.com>
2020-07-14 01:51:20 +0200

    rados: make RadosError type unexported
    In order to avoid external dependencies on implementation details,
    this change replaces RadosError with the unexported radosError. In case
    some application really needs access to the integer value, it can use
    the pattern
      var errno interface{ Errno() int }
      if errors.As(err, errno) { ... errno.Errno() ... }
    
    Signed-off-by: Sven Anderson <sven@redhat.com>

--- rados/errors.go
+++ rados/errors.go
@@ -12,15 +12,11 @@ import (
 	"github.com/ceph/go-ceph/internal/errutil"
 )
 
-// revive:disable:exported Temporarily live with stuttering
+// radosError represents an error condition returned from the Ceph RADOS APIs.
+type radosError int
 
-// RadosError represents an error condition returned from the Ceph RADOS APIs.
-type RadosError int
-
-// revive:enable:exported
-
-// Error returns the error string for the RadosError type.
-func (e RadosError) Error() string {
+// Error returns the error string for the radosError type.
+func (e radosError) Error() string {
 	errno, s := errutil.FormatErrno(int(e))
 	if s == "" {
 		return fmt.Sprintf("rados: ret=%d", errno)
@@ -28,11 +24,15 @@ func (e RadosError) Error() string {
 	return fmt.Sprintf("rados: ret=%d, %s", errno, s)
 }
 
+func (e radosError) Errno() int {
+	return int(e)
+}
+
 func getError(e C.int) error {
 	if e == 0 {
 		return nil
 	}
-	return RadosError(e)
+	return radosError(e)
 }
 
 // getErrorIfNegative converts a ceph return code to error if negative.
@@ -52,15 +52,15 @@ var (
 	ErrNotConnected = errors.New("RADOS not connected")
 )
 
-// Public RadosErrors:
+// Public radosErrors:
 
 const (
 	// ErrNotFound indicates a missing resource.
-	ErrNotFound = RadosError(-C.ENOENT)
+	ErrNotFound = radosError(-C.ENOENT)
 	// ErrPermissionDenied indicates a permissions issue.
-	ErrPermissionDenied = RadosError(-C.EPERM)
+	ErrPermissionDenied = radosError(-C.EPERM)
 	// ErrObjectExists indicates that an exclusive object creation failed.
-	ErrObjectExists = RadosError(-C.EEXIST)
+	ErrObjectExists = radosError(-C.EEXIST)
 
 	// RadosErrorNotFound indicates a missing resource.
 	//
@@ -75,7 +75,7 @@ const (
 // Private errors:
 
 const (
-	errNameTooLong = RadosError(-C.ENAMETOOLONG)
+	errNameTooLong = radosError(-C.ENAMETOOLONG)
 
-	errRange = RadosError(-C.ERANGE)
+	errRange = radosError(-C.ERANGE)
 )
