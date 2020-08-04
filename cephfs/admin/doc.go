344fc784eaa7828496ce0c2aaca9c5f7e8c2e9b5
John Mulligan <jmulligan@redhat.com>
2020-08-04 17:00:16 -0400

    cephfs admin: start a package for convenient administration of cephfs
    These functions are not part of the cephfs APIs but will be based on the
    various command-json features of the 'ceph' command line tool.
    This change only adds a doc.go to stub in the package.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ cephfs/admin/doc.go
@@ -0,0 +1,9 @@
+/*
+Package admin is a convenience layer to support the administration of
+CephFS volumes, subvolumes, etc.
+
+Unlike the cephfs package this API does not map to APIs provided by
+ceph libraries themselves. This API is not yet stable and is subject
+to change.
+*/
+package admin
