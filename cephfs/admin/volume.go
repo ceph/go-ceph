d327bf4f3ec8b24139a5d1762e2012dd195d1621
John Mulligan <jmulligan@redhat.com>
2020-08-05 15:22:19 -0400

    cephfs admin: move volume status to octopus specific files
    The volume status command does not appear to be able to return JSON
    on nautilus. Disable it on anything other than octopus for now.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- cephfs/admin/volume.go
+++ cephfs/admin/volume.go
@@ -9,35 +9,3 @@ func (fsa *FSAdmin) ListVolumes() ([]string, error) {
 	r, s, err := fsa.rawMgrCommand(listVolumesCmd)
 	return parseListNames(r, s, err)
 }
-
-// VolumePool reports on the pool status for a CephFS volume.
-type VolumePool struct {
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-	Type      string `json:"type"`
-	Available uint64 `json:"avail"`
-	Used      uint64 `json:"used"`
-}
-
-// VolumeStatus reports various properties of a CephFS volume.
-// TODO: Fill in.
-type VolumeStatus struct {
-	MDSVersion string       `json:"mds_version"`
-	Pools      []VolumePool `json:"pools"`
-}
-
-func parseVolumeStatus(res []byte, status string, err error) (*VolumeStatus, error) {
-	var vs VolumeStatus
-	err = unmarshalResponseJSON(res, status, err, &vs)
-	return &vs, err
-}
-
-// VolumeStatus returns a VolumeStatus object for the given volume name.
-func (fsa *FSAdmin) VolumeStatus(name string) (*VolumeStatus, error) {
-	r, s, err := fsa.marshalMgrCommand(map[string]string{
-		"fs":     name,
-		"prefix": "fs status",
-		"format": "json",
-	})
-	return parseVolumeStatus(r, s, err)
-}
