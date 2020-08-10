ab49935b5b0aa50b4d83647b96fe86fe102dea7a
John Mulligan <jmulligan@redhat.com>
2020-08-10 10:31:57 -0400

    rbd: rename error type's Errno method to ErrorCode
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- rbd/errors_test.go
+++ rbd/errors_test.go
@@ -15,10 +15,10 @@ func TestRBDError(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, err.Error(), "rbd: ret=39, Directory not empty")
 
-	errno, ok := err.(interface{ Errno() int })
+	errno, ok := err.(interface{ ErrorCode() int })
 	assert.True(t, ok)
 	require.NotNil(t, errno)
-	assert.Equal(t, errno.Errno(), -39)
+	assert.Equal(t, errno.ErrorCode(), -39)
 
 	err = getError(345) // no such errno
 	assert.Error(t, err)
