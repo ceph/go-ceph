f8a69abdde2308b5bcc8811a39a2fd251778037f
Mudit Agarwal <muagarwa@redhat.com>
2020-08-26 12:51:23 +0530

    rbd: add a new version of GetParentInfo()
    GetParentInfo() currently requires the caller to specify the
    string length for various attributes it returns. If the
    specified string length is less than required, it can return
    ERANGE error. Replacing it with a new function which avoids
    this.
    
    Signed-off-by: Mudit Agarwal <muagarwa@redhat.com>

--- rbd/rbd_nautilus_test.go
+++ rbd/rbd_nautilus_test.go
@@ -123,3 +123,80 @@ func TestSparsify(t *testing.T) {
 		assert.Error(t, err)
 	})
 }
+
+func TestGetParent(t *testing.T) {
+	conn := radosConnect(t)
+	poolName := GetUUID()
+	err := conn.MakePool(poolName)
+	assert.NoError(t, err)
+	ioctx, err := conn.OpenIOContext(poolName)
+	assert.NoError(t, err)
+
+	defer func() {
+		ioctx.Destroy()
+		assert.NoError(t, conn.DeletePool(poolName))
+		conn.Shutdown()
+	}()
+
+	imgName := "parent"
+	img, err := Create(ioctx, imgName, testImageSize, testImageOrder, 1)
+	assert.NoError(t, err)
+	defer func() {
+		assert.NoError(t, img.Remove())
+	}()
+
+	img, err = OpenImage(ioctx, imgName, NoSnapshot)
+	assert.NoError(t, err)
+	defer func() {
+		assert.NoError(t, img.Close())
+	}()
+
+	snapName := "mysnap"
+	snapshot, err := img.CreateSnapshot(snapName)
+	assert.NoError(t, err)
+	defer func() {
+		assert.NoError(t, snapshot.Remove())
+	}()
+
+	t.Run("ParentNotAvailable", func(t *testing.T) {
+		_, err := img.GetParent()
+		assert.Error(t, err)
+		assert.Equal(t, err, ErrNotFound)
+	})
+
+	t.Run("ParentAvailable", func(t *testing.T) {
+		cloneName := "child"
+		optionsClone := NewRbdImageOptions()
+		defer optionsClone.Destroy()
+		err := optionsClone.SetUint64(ImageOptionCloneFormat, 2)
+		assert.NoError(t, err)
+
+		// Create a clone of the image using the snapshot.
+		err = CloneImage(ioctx, imgName, snapName, ioctx, cloneName, optionsClone)
+		assert.NoError(t, err)
+		defer func() { assert.NoError(t, RemoveImage(ioctx, cloneName)) }()
+
+		imgNew, err := OpenImage(ioctx, cloneName, NoSnapshot)
+		defer func() {
+			assert.NoError(t, imgNew.Close())
+		}()
+		assert.NoError(t, err)
+
+		parentInfo, err := imgNew.GetParent()
+		assert.NoError(t, err)
+		assert.Equal(t, parentInfo.Image.ImageName, imgName)
+		assert.Equal(t, parentInfo.Snap.SnapName, snapName)
+		assert.Equal(t, parentInfo.Image.PoolName, poolName)
+		// TODO: add a comaprison for snap ID
+	})
+
+	t.Run("ClosedImage", func(t *testing.T) {
+		closedImg, err := Create(ioctx, "someImage", testImageSize, testImageOrder, 1)
+		assert.NoError(t, err)
+		defer func() {
+			assert.NoError(t, closedImg.Remove())
+		}()
+		_, err = closedImg.GetParent()
+		assert.Error(t, err)
+	})
+}
