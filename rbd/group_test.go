cd0a5bfe019eb49ce8b6e347919ebc299f71c5e7
John Mulligan <jmulligan@redhat.com>
2021-01-20 14:09:09 -0500

    rbd: add GroupImageAdd, GroupImageRemove, and GroupImageRemoveByID
    * Add GroupImageAdd implementing rbd_group_image_add
    * Add GroupImageRemove implementing rbd_group_image_remove
    * Add GroupImageRemoveByID implementing rbd_group_image_remove_by_id
    
    These functions allow managing the images that are part of a group.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- rbd/group_test.go
+++ rbd/group_test.go
@@ -113,3 +113,130 @@ func TestGroupList(t *testing.T) {
 		GroupList(nil)
 	})
 }
+
+func TestGroupImageAdd(t *testing.T) {
+	conn := radosConnect(t)
+	require.NotNil(t, conn)
+	defer conn.Shutdown()
+
+	poolname := GetUUID()
+	err := conn.MakePool(poolname)
+	require.NoError(t, err)
+	defer conn.DeletePool(poolname)
+
+	ioctx, err := conn.OpenIOContext(poolname)
+	require.NoError(t, err)
+	defer ioctx.Destroy()
+
+	name := GetUUID()
+	options := NewRbdImageOptions()
+	assert.NoError(t,
+		options.SetUint64(ImageOptionOrder, uint64(testImageOrder)))
+	err = CreateImage(ioctx, name, testImageSize, options)
+	require.NoError(t, err)
+
+	err = GroupCreate(ioctx, "grone")
+	assert.NoError(t, err)
+
+	err = GroupImageAdd(ioctx, "grone", ioctx, name)
+	assert.NoError(t, err)
+
+	err = GroupImageAdd(ioctx, "badGroup", ioctx, name)
+	assert.Error(t, err)
+
+	assert.Panics(t, func() {
+		GroupImageAdd(nil, "invalid", ioctx, "foobar")
+	})
+	assert.Panics(t, func() {
+		GroupImageAdd(ioctx, "invalid", nil, "foobar")
+	})
+}
+
+func TestGroupImageRemove(t *testing.T) {
+	conn := radosConnect(t)
+	require.NotNil(t, conn)
+	defer conn.Shutdown()
+
+	poolname := GetUUID()
+	err := conn.MakePool(poolname)
+	require.NoError(t, err)
+	defer conn.DeletePool(poolname)
+
+	ioctx, err := conn.OpenIOContext(poolname)
+	require.NoError(t, err)
+	defer ioctx.Destroy()
+
+	name := GetUUID()
+	options := NewRbdImageOptions()
+	assert.NoError(t,
+		options.SetUint64(ImageOptionOrder, uint64(testImageOrder)))
+	err = CreateImage(ioctx, name, testImageSize, options)
+	require.NoError(t, err)
+
+	err = GroupCreate(ioctx, "grone")
+	assert.NoError(t, err)
+
+	err = GroupImageAdd(ioctx, "grone", ioctx, name)
+	assert.NoError(t, err)
+
+	err = GroupImageRemove(ioctx, "grone", ioctx, name)
+	assert.NoError(t, err)
+
+	err = GroupImageRemove(ioctx, "badGroup", ioctx, name)
+	assert.Error(t, err)
+
+	assert.Panics(t, func() {
+		GroupImageRemove(nil, "invalid", ioctx, "foobar")
+	})
+	assert.Panics(t, func() {
+		GroupImageRemove(ioctx, "invalid", nil, "foobar")
+	})
+}
+
+func TestGroupImageRemoveByID(t *testing.T) {
+	conn := radosConnect(t)
+	require.NotNil(t, conn)
+	defer conn.Shutdown()
+
+	poolname := GetUUID()
+	err := conn.MakePool(poolname)
+	require.NoError(t, err)
+	defer conn.DeletePool(poolname)
+
+	ioctx, err := conn.OpenIOContext(poolname)
+	require.NoError(t, err)
+	defer ioctx.Destroy()
+
+	name := GetUUID()
+	options := NewRbdImageOptions()
+	assert.NoError(t,
+		options.SetUint64(ImageOptionOrder, uint64(testImageOrder)))
+	err = CreateImage(ioctx, name, testImageSize, options)
+	require.NoError(t, err)
+
+	err = GroupCreate(ioctx, "grone")
+	assert.NoError(t, err)
+
+	err = GroupImageAdd(ioctx, "grone", ioctx, name)
+	assert.NoError(t, err)
+
+	img, err := OpenImage(ioctx, name, NoSnapshot)
+	assert.NoError(t, err)
+	imageID, err := img.GetId()
+	assert.NoError(t, err)
+	err = img.Close()
+	assert.NoError(t, err)
+
+	err = GroupImageRemoveByID(ioctx, "grone", ioctx, imageID)
+	assert.NoError(t, err)
+
+	err = GroupImageRemoveByID(ioctx, "badGroup", ioctx, imageID)
+	assert.Error(t, err)
+
+	assert.Panics(t, func() {
+		GroupImageRemoveByID(nil, "invalid", ioctx, "foobar")
+	})
+	assert.Panics(t, func() {
+		GroupImageRemoveByID(ioctx, "invalid", nil, "foobar")
+	})
+}
