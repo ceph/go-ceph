b5c1000b7a2ef01dab7e8baa06027385ec2f3fd6
John Mulligan <jmulligan@redhat.com>
2021-01-25 15:21:15 -0500

    rbd: add GroupSnapRollbackWithProgress function
    * Add GroupSnapRollbackWithProgress implementing rbd_group_snap_rollback_with_progress
    
    This has supporting code basically the same as our other existing
    callback functions.
    Tests too.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- rbd/group_snap_test.go
+++ rbd/group_snap_test.go
@@ -153,6 +153,69 @@ func TestGroupSnapshots(t *testing.T) {
 		err = GroupSnapRemove(ioctx, gname, snapname)
 		assert.NoError(t, err)
 	})
+	t.Run("groupSnapRollbackWithProgress", func(t *testing.T) {
+		img, err := OpenImage(ioctx, name1, NoSnapshot)
+		assert.NoError(t, err)
+		_, err = img.WriteAt([]byte("SAY CHEESE"), 0)
+		assert.NoError(t, err)
+		_, err = img.WriteAt([]byte("AND SMILE_"), 10240)
+		assert.NoError(t, err)
+		err = img.Close()
+		assert.NoError(t, err)
+
+		snapname := "snap2r"
+		err = GroupSnapCreate(ioctx, gname, snapname)
+		assert.NoError(t, err)
+
+		img, err = OpenImage(ioctx, name1, NoSnapshot)
+		assert.NoError(t, err)
+		_, err = img.WriteAt([]byte("GOODBYE WORLD"), 0)
+		assert.NoError(t, err)
+		_, err = img.WriteAt([]byte("_THIS_IS_ALL_"), 10240)
+		assert.NoError(t, err)
+		_, err = img.WriteAt([]byte("I_HAVE_TO_SAY"), 11240)
+		assert.NoError(t, err)
+		err = img.Close()
+		assert.NoError(t, err)
+
+		img, err = OpenImage(ioctx, name2, NoSnapshot)
+		assert.NoError(t, err)
+		_, err = img.WriteAt([]byte("3333333333333"), 0)
+		assert.NoError(t, err)
+		err = img.Close()
+		assert.NoError(t, err)
+
+		cc := 0
+		cb := func(offset, total uint64, v interface{}) int {
+			cc++
+			val := v.(int)
+			assert.Equal(t, 0, val)
+			assert.Equal(t, uint64(1), total)
+			return 0
+		}
+		err = GroupSnapRollbackWithProgress(ioctx, gname, snapname, cb, 0)
+		assert.NoError(t, err)
+		assert.GreaterOrEqual(t, cc, 2)
+
+		b := make([]byte, 8)
+		img, err = OpenImage(ioctx, name1, NoSnapshot)
+		assert.NoError(t, err)
+		_, err = img.ReadAt(b, 0)
+		assert.NoError(t, err)
+		err = img.Close()
+		assert.NoError(t, err)
+		assert.Equal(t, []byte("SAY CHEE"), b)
+
+		img, err = OpenImage(ioctx, name2, NoSnapshot)
+		assert.NoError(t, err)
+		_, err = img.ReadAt(b, 0)
+		assert.NoError(t, err)
+		err = img.Close()
+		assert.NoError(t, err)
+		assert.Equal(t, []byte("\x00\x00\x00\x00\x00\x00\x00\x00"), b)
+
+		err = GroupSnapRemove(ioctx, gname, snapname)
+		assert.NoError(t, err)
 	})
 	t.Run("invalidIOContext", func(t *testing.T) {
 		assert.Panics(t, func() {
@@ -170,5 +233,13 @@ func TestGroupSnapshots(t *testing.T) {
 		assert.Panics(t, func() {
 			GroupSnapRollback(nil, gname, "foo")
 		})
+		assert.Panics(t, func() {
+			cb := func(o, t uint64, v interface{}) int { return 0 }
+			GroupSnapRollbackWithProgress(nil, gname, "foo", cb, nil)
+		})
+	})
+	t.Run("invalidCallback", func(t *testing.T) {
+		err := GroupSnapRollbackWithProgress(ioctx, gname, "foo", nil, nil)
+		assert.Error(t, err)
 	})
 }
