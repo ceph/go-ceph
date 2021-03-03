ac2026b118d50eb0c0d54297878792ffcca11867
John Mulligan <jmulligan@redhat.com>
2021-03-03 14:44:17 -0500

    rbd admin: add tests for the LevelSpec type
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ rbd/admin/admin_test.go
@@ -0,0 +1,32 @@
+// +build !nautilus
+
+package admin
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLevelSpec(t *testing.T) {
+	ls := NewLevelSpec("bob", "", "")
+	assert.Equal(t, "bob/", ls.spec)
+
+	ls = NewLevelSpec("bob", "", "foo")
+	assert.Equal(t, "bob/foo", ls.spec)
+
+	ls = NewLevelSpec("bob", "ns", "foo")
+	assert.Equal(t, "bob/ns/foo", ls.spec)
+
+	ls = NewLevelSpec("bob", "ns", "")
+	assert.Equal(t, "bob/ns/", ls.spec)
+}
+
+func TestRawLevelSpec(t *testing.T) {
+	rls := NewRawLevelSpec("foo/bar")
+	assert.Equal(t, "foo/bar", rls.spec)
+
+	// NewRawLevelSpec takes whatever junk it's given and does not validate
+	rls = NewRawLevelSpec("totally! invalid! haha. ha...")
+	assert.Equal(t, "totally! invalid! haha. ha...", rls.spec)
+}
