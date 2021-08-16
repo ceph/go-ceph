64e826d8f71fa55a397b359ad9072baaeda98850
John Mulligan <jmulligan@redhat.com>
2021-08-16 12:58:00 -0400

    rgw admin: import testify suite as tsuite
    This avoids import shadowing elsewhere in radosgw_test.go.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- rgw/admin/radosgw_test.go
+++ rgw/admin/radosgw_test.go
@@ -14,11 +14,11 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/stretchr/testify/suite"
+	tsuite "github.com/stretchr/testify/suite"
 )
 
 type RadosGWTestSuite struct {
-	suite.Suite
+	tsuite.Suite
 	endpoint       string
 	accessKey      string
 	secretKey      string
