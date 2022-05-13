060569a74900348ba437bf2d3dc05b17d7ba0765
Sanford Miller <smiller@digitalocean.com>
2022-05-13 18:19:38 -0400

    rbd: expose rbd_snap_rename to Go clients
    This implements the following method:
    	(snapshot *Snapshot) Rename(destName string) error
    
    Signed-off-by: Sanford Miller <smiller@digitalocean.com>

---
+++ rbd/snapshot_rename_test.go
@@ -0,0 +1,60 @@
+//go:build ceph_preview
+// +build ceph_preview
+
+package rbd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRenameSnapshot(t *testing.T) {
+	conn := radosConnect(t)
+	poolName := GetUUID()
+	err := conn.MakePool(poolName)
+	require.NoError(t, err)
+	ioctx, err := conn.OpenIOContext(poolName)
+	require.NoError(t, err)
+
+	name := GetUUID()
+	err = CreateImage(ioctx, name, 1<<22, NewRbdImageOptions())
+	require.NoError(t, err)
+
+	// create snapshot
+	img, err := OpenImage(ioctx, name, NoSnapshot)
+	require.NoError(t, err)
+	snapshotName := "mysnap"
+	snapshot, err := img.CreateSnapshot(snapshotName)
+	require.NoError(t, err)
+
+	// verify snapshot opens
+	snapImg, err := OpenImage(ioctx, name, snapshotName)
+	require.NoError(t, err)
+	err = snapImg.Close()
+	require.NoError(t, err)
+
+	// rename snapshot
+	newSnapshotName := "myrenamedsnap"
+	err = snapshot.Rename(newSnapshotName)
+	require.NoError(t, err)
+
+	// verify snapshot still opens
+	snapImg, err = OpenImage(ioctx, name, newSnapshotName)
+	require.NoError(t, err)
+	err = snapImg.Close()
+	require.NoError(t, err)
+
+	err = snapshot.Remove()
+	require.NoError(t, err)
+
+	err = img.Close()
+	require.NoError(t, err)
+	err = img.Remove()
+	require.NoError(t, err)
+
+	ioctx.Destroy()
+	err = conn.DeletePool(poolName)
+	require.NoError(t, err)
+	conn.Shutdown()
+}
