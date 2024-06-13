9559e694953b55f42e53aa0de7efd0232bff92b1
Simon Jürgensmeyer <simon.juergensmeyer@hetzner-cloud.de>
2024-06-13 11:41:08 +0200

    rgw/admin: add ErrNoSuchSubUser
    Signed-off-by: Simon Jürgensmeyer <simon.juergensmeyer@hetzner-cloud.de>

--- rgw/admin/errors_test.go
+++ rgw/admin/errors_test.go
@@ -8,11 +8,16 @@ import (
 )
 
 var (
-	fakeGetUserError = []byte(`{"Code":"NoSuchUser","RequestId":"tx0000000000000000005a9-00608957a2-10496-my-store","HostId":"10496-my-store-my-store"}`)
+	fakeGetUserError    = []byte(`{"Code":"NoSuchUser","RequestId":"tx0000000000000000005a9-00608957a2-10496-my-store","HostId":"10496-my-store-my-store"}`)
+	fakeGetSubUserError = []byte(`{"Code":"NoSuchSubUser","RequestId":"tx0000000000000000005a9-00608957a2-10496-my-store","HostId":"10496-my-store-my-store"}`)
 )
 
 func TestHandleStatusError(t *testing.T) {
 	err := handleStatusError(fakeGetUserError)
 	assert.Error(t, err)
 	assert.True(t, errors.Is(err, ErrNoSuchUser), err)
+
+	err = handleStatusError(fakeGetSubUserError)
+	assert.Error(t, err)
+	assert.True(t, errors.Is(err, ErrNoSuchSubUser), err)
 }
