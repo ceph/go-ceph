5c95766a1ebfbd95e2ba3c0aec8e7b45c12f252a
Robert Vasek <robert.vasek@clyso.com>
2024-10-18 17:57:42 +0200

    rados: implement rados_getaddrs
    This commit implements binding for rados_getaddrs.
    
    Includes a unit test.
    
    Signed-off-by: Robert Vasek <robert.vasek@clyso.com>

---
+++ rados/rados_getaddrs_test.go
@@ -0,0 +1,15 @@
+//go:build ceph_preview
+
+package rados
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+func (suite *RadosTestSuite) TestGetAddrs() {
+	suite.SetupConnection()
+
+	addrs, err := suite.conn.GetAddrs()
+	assert.NoError(suite.T(), err)
+	assert.NotEmpty(suite.T(), addrs, "rados_getaddrs")
+}
