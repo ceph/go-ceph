cfee8483f976d436d602474adac37070ffff0920
John Mulligan <jmulligan@redhat.com>
2025-04-07 13:50:31 -0400

    admin/smb: expand the general smb package doc comment
    Add a small commentary on how the smb module is expected to be used.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- common/admin/smb/doc.go
+++ common/admin/smb/doc.go
@@ -1,5 +1,16 @@
 /*
 Package smb from common/admin contains a set of APIs used to interact
 with and administer SMB support for ceph clusters.
+
+The Ceph smb mgr module is based on the concept of resources. Resource
+descriptions are used to create, update, or delete configuration state in
+the Ceph cluster and the Ceph cluster will attempt to configure SMB Servers
+based on these resources.
+
+Resource types include Cluster, JoinAuth, UsersAndGroups, and Share. To
+modify the state on the Ceph cluster use the Apply function. To query
+the state of the resources on the Ceph cluster use the Show function.
+Resources that are to be deleted should have an Intent value of Removed.
+Resources being updated or created must have an Intent value of Present.
 */
 package smb
